3d4ba6923918a8de5a68bfd4571a5f24fe4b650d
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-05 09:41:29 +0530

    Create Battery_Condition_Status.c

---
+++ Battery_Condition_Status.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "Battery_Condition_Status.h"
+#include "WarningChecker.h"
+
+void displayWarning(const char* param_name, int* message_index) {
+    printf("Checking parameters values:\n");
+    printf("%s: %s\n", param_name, messages[language][message_index]); 
+}
+
+int messageIndexMapping(float value, ParameterConfig config) {
+    int message_index = 0;
+    message_index = isLessThan(value, config);
+    if (message_index != 0) {
+        return message_index;
+    }
+    message_index = isGreaterThan(value, config);
+    if (message_index != 0) {
+        return message_index;
+    }
+    return warningChecker(value, config);
+}
+
+void parameterValidation(const char* param_name, float value, ParameterConfig config) {
+    int message_index = messageIndexMapping(value, config);
+    displayWarning(param_name, message_index);
+}
+
