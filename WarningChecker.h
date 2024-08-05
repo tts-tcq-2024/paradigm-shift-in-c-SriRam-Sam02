781ffc4958eec9e72fed6ed71c01eee2ea5e968e
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-05 09:41:48 +0530

    Create WarningChecker.h

---
+++ WarningChecker.h
@@ -0,0 +1,8 @@
+#ifndef WARNING_CHECKER_H
+#define WARNING_CHECKER_H
+
+int isLessThan(float value, ParameterConfig config);
+int isGreaterThan(float value, ParameterConfig config);
+int warningChecker(float value, ParameterConfig config);
+
+#endif
