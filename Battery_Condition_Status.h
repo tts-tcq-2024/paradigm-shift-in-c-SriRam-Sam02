d4d9850aeff12e4c940af2048ad4d1255c8e3f4c
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-05 10:01:52 +0530

    Update Battery_Condition_Status.h

--- Battery_Condition_Status.h
+++ Battery_Condition_Status.h
@@ -29,4 +29,8 @@ const char *messages[2][5] = {
     }
 };
 
+void displayWarning(const char* param_name, int message_index);
+int messageIndexMapping(float value, ParameterConfig config);
+void parameterValidation(const char* param_name, float value, ParameterConfig config);
+
 #endif
