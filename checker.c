dc3bc8ee295f61e6b99c5a4a3014dc987feed934
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-11 22:33:58 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,25 +1,5 @@
 #include "Battery_Condition_Status.h"
 
-enum Language { ENGLISH, GERMAN };
-enum Language language = 1;
-
-const char *messages[2][5] = {
-    {
-        "Normal",
-        "Warning: Approaching discharge",
-        "Warning: Approaching charge-peak",
-        "Alarm: Low limit breached",
-        "Alarm: High limit breached"
-    },
-    {
-        "Normal",
-        "Warnung: Entladung nähert sich",
-        "Warnung: Ladehöhe nähert sich",
-        "Alarm: Untergrenze überschritten",
-        "Alarm: Obergrenze überschritten"
-    }
-};
-
 ParameterConfig SoC = {20, 80, 0.05, true};
 ParameterConfig Temperature = {0, 45, 0.05, true};
 ParameterConfig Charge_rate = {0, 0.8, 0.05, true};
