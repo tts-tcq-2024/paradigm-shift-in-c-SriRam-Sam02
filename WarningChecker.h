8cef2d8b2112e37a54652c20b2edb9d3dab02b83
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-11 19:52:09 +0530

    Update WarningChecker.h

--- WarningChecker.h
+++ WarningChecker.h
@@ -1,8 +1,6 @@
 #ifndef WARNING_CHECKER_H
 #define WARNING_CHECKER_H
 
-#include "Battery_Condition_Status.h"
-
 int isLessThan(float value, ParameterConfig config);
 int isGreaterThan(float value, ParameterConfig config);
 int warningChecker(float value, ParameterConfig config);
