ab8fa31efe414bb8e9c91cc7aa0dcc0b5a720102
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-11 22:28:19 +0530

    Update Battery_Condition_Status.h

--- Battery_Condition_Status.h
+++ Battery_Condition_Status.h
@@ -1,7 +1,6 @@
+#ifndef BATTERY_CONDITION_STATUS_H
+#define BATTERY_CONDITION_STATUS_H
 #include <stdbool.h>
-
-enum Language { ENGLISH, GERMAN };
-enum Language language = 1;
 
 typedef struct {
     float min;
@@ -10,23 +9,6 @@ typedef struct {
     bool enable_warning;
 } ParameterConfig;
 
-const char *messages[2][5] = {
-    {
-        "Normal",
-        "Warning: Approaching discharge",
-        "Warning: Approaching charge-peak",
-        "Alarm: Low limit breached",
-        "Alarm: High limit breached"
-    },
-    {
-        "Normal",
-        "Warnung: Entladung nähert sich",
-        "Warnung: Ladehöhe nähert sich",
-        "Alarm: Untergrenze überschritten",
-        "Alarm: Obergrenze überschritten"
-    }
-};
-
 int isLessThan(float value, ParameterConfig config);
 int isGreaterThan(float value, ParameterConfig config);
 int warningChecker(float value, ParameterConfig config);
@@ -34,3 +16,4 @@ void displayWarning(const char* param_name, int message_index);
 int messageIndexMapping(float value, ParameterConfig config);
 void parameterValidation(const char* param_name, float value, ParameterConfig config);
 
+#endif
