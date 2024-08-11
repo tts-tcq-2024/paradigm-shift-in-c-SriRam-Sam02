06822d12b792358952c9f9d602ec19e199195728
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-11 20:24:41 +0530

    Update Battery_Condition_Status.c

--- Battery_Condition_Status.c
+++ Battery_Condition_Status.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include "Battery_Condition_Status.h"
-#include "WarningChecker.h"
+
+
+int isLessThan(float value, ParameterConfig config) {
+    return (value < config.min) ? 3 : 0;
+}
+
+int isGreaterThan(float value, ParameterConfig config) {
+    return (value > config.max) ? 4 : 0;
+}
+
+int warningChecker(float value, ParameterConfig config) {
+    
+    float warning_min = config.min + (config.max * config.warning_tolerance);
+    float warning_max = config.max - (config.max * config.warning_tolerance);
+    if (value < warning_min) {
+        return 1; 
+    }
+    if (value > warning_max) {
+        return 2; 
+    }
+    return 0;  
+}
 
 void displayWarning(const char* param_name, int message_index) {
     printf("Checking parameters values:\n");
