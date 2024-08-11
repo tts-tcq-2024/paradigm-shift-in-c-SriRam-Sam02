f951f3edae5b5f949481d4bc913484378bd16ac5
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-11 20:16:01 +0530

    Update Battery_Condition_Status.c

--- Battery_Condition_Status.c
+++ Battery_Condition_Status.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "Battery_Condition_Status.h"
+#include "WarningChecker.h"
 
 void displayWarning(const char* param_name, int message_index) {
     printf("Checking parameters values:\n");
