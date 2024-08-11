0c2e77d88413d362c2c9ffdd98e61cc919f8ee23
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-11 23:03:05 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -10,12 +10,12 @@ ParameterConfig Charge_rate = {0, 0.8, 0.05, true};
 int main()
 {
     float config_SoC = 90;
-   // float config_Temperature = 20;
-   // float config_Charge_rate = 0.5;
+    float config_Temperature = 20;
+    float config_Charge_rate = 0.5;
     
     parameterValidation("SoC", config_SoC, SoC);
-   // parameterValidation("Temperature", config_Temperature, Temperature);
-   // parameterValidation("Charge_rate", config_Charge_rate, Charge_rate);
+    parameterValidation("Temperature", config_Temperature, Temperature);
+    parameterValidation("Charge_rate", config_Charge_rate, Charge_rate);
 
     return 0;  
 }
